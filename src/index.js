6ee47f7ff465357a7cf746b2dcf9fabaa053c6c7
Fedytnik, Nikolay <n.fedytnik@itransition.com>
2019-03-19 21:52:54 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,33 +1,84 @@
 module.exports = function getZerosCount(number, base) {
-  if (number > 1) {
-    let factorial = algorithmP(2, number);
-    return getCountWhileConvert(factorial, base);
+  let primes = eratosfen(base);
+  let multiprimes = getMultiPrimes(base, primes);
+  let multiPrimesPower = getMiltiPrimePower(multiprimes);
+  let zeroCount;
+  for (const pair of multiPrimesPower.entries()) {
+    if (zeroCount === undefined) {
+      zeroCount = Math.floor(powerOf(pair[0], number) / pair[1])
+    } else {
+      zeroCount = Math.min(zeroCount, Math.floor(powerOf(pair[0], number) / pair[1]))
+    }
   }
-  return 0;
+
+  return zeroCount;
 }
 
-function algorithmP(p1, p2) {
-  if (p2 > p1) {
-    let half = halfsOfP(p1, p2);
-    return algorithmP(p1, half) * algorithmP(half + 1, p2)
+function getMultiPrimes(n, primes) {
+  let multiprimes = [];
+  while (n !== 1) {
+    primes.forEach(function(prime) {
+      if (prime > n) {
+        return true;
+      }
+      if (n % prime === 0) {
+        multiprimes.push(prime);
+        n = n / prime;
+        return true;
+      }
+    });
   }
-  return p1;
+
+  return multiprimes;
 }
 
-function getCountWhileConvert(n, base) {
-  let zerosCount = 0;
-  let b = Math.floor(n / base);
-  let k = n % base;
-  while (k === 0) {
-    zerosCount++;
-    n = b;
-    b = Math.floor(n / base);
-    k = n % base;
-  }
+function getMiltiPrimePower(multiprimes) {
+  multiprimes = multiprimes.sort();
+  let multiPrimesPower = new Map();
+  let currentPrime = multiprimes[0];
+  let primePower = 0;
   
-  return zerosCount;
+  for (let index = 0; index < multiprimes.length; index++) {
+    if (multiprimes[index] !== currentPrime) {
+      multiPrimesPower.set(currentPrime, primePower);
+      currentPrime = multiprimes[index];
+      primePower = 1;
+    } else {
+      primePower++;
+    }
+  }
+  multiPrimesPower.set(currentPrime, primePower);
+
+  return multiPrimesPower;
 }
 
-function halfsOfP(p1, p2) {
-  return p1 + Math.floor((p2 - p1) / 2);
+function powerOf(a, n) {
+  let power = 0;
+  let powerA = a;
+  while (n / powerA > 1) {
+    power += Math.floor(n / powerA);
+    powerA *= a;
+  }
+
+  return power;
+}
+
+function eratosfen(n) {
+  let sieve = Array(n + 1).fill(true);
+  for (let index = 2; index <= Math.sqrt(n) + 1; index++) {
+    if (sieve[index] === true) {
+      for (let j = index * index; j < n; j += index) {
+        sieve[j] = false;
+      }
+    }
+  }
+
+  let primes = [];
+  for (let index = 2; index <= n; index++){
+    if (sieve[index]) {
+      primes.push(index);
+    }
+  }
+
+  return primes;
 }
